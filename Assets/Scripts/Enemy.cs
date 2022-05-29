c53a0d5f3cf45b337166bf9f8266c920905c9cc1
marshalonah <marshalonah@gmail.com>
2022-05-29 07:56:59 +0100

    enemy Bird movement

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    [SerializeField] private int speed;
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move();
+    }
+
+    public void Move()
+    {
+        transform.position += Vector3.left * speed * Time.deltaTime;
+    }
+}
