d670ed89da8b4cfa16aaf822f817a074feb8b90d
marshalonah <marshalonah@gmail.com>
2022-05-29 19:21:22 +0100

    modified the spawning process with coroutine

--- Assets/Scripts/SpawnManager.cs
+++ Assets/Scripts/SpawnManager.cs
@@ -5,10 +5,11 @@ using UnityEngine;
 public class SpawnManager : MonoBehaviour
 {
     [SerializeField] float maxY;
+    [SerializeField] float spawnInterval;
     public GameObject[] Birds;
     void Start()
     {
-        SpawnBirds();
+        StartSpawningBirds();
     }
 
     // Update is called once per frame
@@ -17,12 +18,33 @@ public class SpawnManager : MonoBehaviour
 
     }
 
-    public void SpawnBirds()
+    public void InstantiateBirds()
     {
         int randomBirdIndex = Random.Range(0, Birds.Length);
         float randomYpos = Random.Range(-maxY, maxY);
 
         Vector3 BirdPosition = new Vector3(transform.position.x, randomYpos, transform.position.z);
         Instantiate(Birds[randomBirdIndex], BirdPosition, transform.rotation);
+    }
+
+    IEnumerator SpawnBirds()
+    {
+        yield return new WaitForSeconds(1f);
+
+        while (true)
+        {
+            InstantiateBirds();
+            yield return new WaitForSeconds(spawnInterval);
+        }
+    }
+
+    public void StartSpawningBirds()
+    {
+        StartCoroutine(SpawnBirds());
+    }
+
+    public void StopSpawningBirds()
+    {
+        StopCoroutine(SpawnBirds());
     }
 }
