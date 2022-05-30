0543d459cd653ca695da5530cfc930db29e055e8
marshalonah <marshalonah@gmail.com>
2022-05-30 08:49:51 +0100

    added Game manager and a score system

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+
+    public static GameManager instance;
+    public GameState gameState = GameState.Stopped;
+    public int score;
+    // Start is called before the first frame update
+
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+    }
+
+    void Start()
+    {
+        gameState = GameState.Playing;
+        StartCoroutine(IncrementScore());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    IEnumerator IncrementScore()
+    {
+        while (gameState == GameState.Playing)
+        {
+            score++;
+            print(score);
+            yield return new WaitForSeconds(1f);
+        }
+    }
+}
